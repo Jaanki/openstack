4331962ee35d95ba4f21c5793f54ef1bac7fc779
Janki <8556738+Jaanki@users.noreply.github.com>
2018-06-20 19:27:15 +0530

    Update create_vxlan_network.sh

--- scripts/create_vxlan_network.sh
+++ scripts/create_vxlan_network.sh
@@ -1,2 +1,3 @@
+source ~/overcloudrc
 openstack network create vxlan1 && echo "vxlan1 network created" || echo "vxlan1 network creation failed"
 neutron subnet-create --name vxlan1-subnet vxlan1 192.168.99.0/24
