0d5b37de36f57fbd79b229c9da45467827ec0b31
Janki Chhatbar <jchhatba@redhat.com>
2018-11-20 12:34:24 +0530

    Edit some scripts

---
+++ scripts/check_odls.sh
@@ -0,0 +1,29 @@
+source ~/stackrc
+
+OVERCLOUD_NODES=$(($(openstack server list | awk 'NR>=4 {print $4}' | wc -l) - 1))
+
+cat > check_odl.sh <<'EOF'
+RED='\033[0;31m'
+NC='\033[0m'
+set -e
+if [ "healthy" == $(sudo docker inspect opendaylight_api --format={{.State.Health.Status}}) ]; then 
+  echo "ODL container is healthy"
+else
+  echo -e "ODL container is ${RED}NOT healthy${NC}. Please check logs"
+  exit
+fi
+
+#TUNNEL_NUMBER=let 
+EOF
+
+chmod +x check_odl.sh
+
+for i in $(openstack server list | awk 'NR>=4 {print $4 $8}' | grep controller)
+do
+  node=$(echo $i | awk -F 'ctlplane=' '{print $1}')
+  ip=$(echo $i | awk -F 'ctlplane=' '{print $2}')
+  echo -e "\nworking on" $node
+  ssh -q -oStrictHostKeyChecking=no heat-admin@$ip "if [ -f check_odl.sh ]; then rm check_odl.sh; fi"
+  scp -oStrictHostKeyChecking=no check_odl.sh heat-admin@$ip:/home/heat-admin
+  ssh -q -oStrictHostKeyChecking=no heat-admin@$ip "./check_odl.sh; echo 'tunnel ports are'; sudo ovs-vsctl show | grep 'Port \"tun' | wc -l; grep -B 2 -r 'java.io.FileNotFoundException: /opt/opendaylight/etc/opendaylight/karaf/' /var/log/containers/opendaylight/karaf/logs/karaf.log || echo 'no files missing'" #; echo 'stopping ODL contaner and waiting for a minite'; sudo docker stop opendaylight_api; sudo docker ps | grep opendaylight_api; sleep 60; echo 'starting ODL container'; sudo docker start opendaylight_api; sudo docker ps | grep opendaylight_api; echo 'waiting 2 mins for ODL to come up'; sleep 120; sudo docker ps | grep opendaylight_api; ./check_odl.sh"
+done
--- scripts/odltools.sh
+++ scripts/odltools.sh
@@ -9,8 +9,13 @@ odl_ip=$(sudo hiera -c /etc/puppet/hiera.yaml opendaylight::odl_bind_ip)
 port=$(sudo hiera -c /etc/puppet/hiera.yaml opendaylight::odl_rest_port)
 username=$(sudo hiera -c /etc/puppet/hiera.yaml opendaylight::username)
 password=$(sudo hiera -c /etc/puppet/hiera.yaml opendaylight::password)
+
+if [[ $odl_ip =~ .*:.* ]]; then
+  odl_ip="[${odl_ip}]"
+fi
+
 ##TODO: Get port, username, password from the node
 python -m odltools model get --ip $odl_ip --port $port --user $username --pw $password -p ~/odltools/odl_dumps
-python -m odltools show cluster-info --ip $odl_ip --port $port --user $username --pw $password >> ~/odltools/cluster.info
-python -m odltools analyze tunnels --ip $odl_ip --port $port --user $username --pw $password --path ~/odltools/tunnels >> ~/odltools/tunnels_info.log
-python -m odltools show eos --ip $odl_ip --port $port --user $username --pw $password --path ~/odltools/eos 
+python -m odltools netvirt show cluster-info --ip $odl_ip --port $port --user $username --pw $password >> ~/odltools/cluster.info
+python -m odltools netvirt analyze tunnels --ip $odl_ip --port $port --user $username --pw $password --path ~/odltools/tunnels >> ~/odltools/tunnels_info.log
+python -m odltools netvirt show eos --ip $odl_ip --port $port --user $username --pw $password --path ~/odltools/eos 
