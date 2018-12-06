bb69936d9a672f64f104bf089b98a463ee94a693
Janki Chhatbar <jchhatba@redhat.com>
2018-12-06 19:32:42 +0530

    Edit some files

--- scripts/check_odls.sh
+++ scripts/check_odls.sh
@@ -27,9 +27,4 @@ do
   ssh -q -oStrictHostKeyChecking=no heat-admin@$ip "if [ -f check_odl.sh ]; then rm check_odl.sh; fi"
   scp -oStrictHostKeyChecking=no check_odl.sh heat-admin@$ip:/home/heat-admin
   ssh -q -oStrictHostKeyChecking=no heat-admin@$ip "./check_odl.sh; echo 'tunnel ports are'; sudo ovs-vsctl show | grep 'Port \"tun' | wc -l; grep -B 2 -r 'java.io.FileNotFoundException: /opt/opendaylight/etc/opendaylight/karaf/' /var/log/containers/opendaylight/karaf/logs/karaf.log || echo 'no files missing'" #; echo 'stopping ODL contaner and waiting for a minite'; sudo docker stop opendaylight_api; sudo docker ps | grep opendaylight_api; sleep 60; echo 'starting ODL container'; sudo docker start opendaylight_api; sudo docker ps | grep opendaylight_api; echo 'waiting 2 mins for ODL to come up'; sleep 120; sudo docker ps | grep opendaylight_api; ./check_odl.sh"
-
-  echo "working on" $node
-  ssh -oStrictHostKeyChecking=no heat-admin@$ip "if [ -f ping_gw.sh ]; then rm ping_gw.sh; fi"
-  scp -oStrictHostKeyChecking=no check_odl.sh heat-admin@$ip:/home/heat-admin
-  ssh -oStrictHostKeyChecking=no heat-admin@$ip "./check_odl.sh"
 done
